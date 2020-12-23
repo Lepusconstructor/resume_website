9b94f25f14a541c80cfa6eab6212d9f218649073
honeyyusuf <yusufhon0@gmail.com>
2020-12-23 13:24:11 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import JssProvider from "react-jss/lib/JssProvider";
+import { createGenerateClassName } from "@material-ui/core/styles";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+import Demo from "./Demo.js";
+
+// const faces = [
+//   "http://i.pravatar.cc/300?img=1",
+//   "http://i.pravatar.cc/300?img=2",
+//   "http://i.pravatar.cc/300?img=3",
+//   "http://i.pravatar.cc/300?img=4"
+// ];
+
+const muiBaseTheme = createMuiTheme();
+
+const generateClassName = createGenerateClassName({
+  dangerouslyUseGlobalCSS: true
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <JssProvider generateClassName={generateClassName}>
+      <MuiThemeProvider
+        theme={createMuiTheme({
+          typography: {
+            useNextVariants: true
+          },
+          overrides: Demo.getTheme(muiBaseTheme)
+        })}
+      >
+        <Demo />
+      </MuiThemeProvider>
+    </JssProvider>
   );
 }
+
 
 export default App;
